42f4ee7c89a711634d4457d056d9856216b8ef36
tylerfaulkner <tylersfaulkner@gmail.com>
2023-01-17 12:28:12 -0600

    Made Fixes, Added MinIO Client

--- build.sbt
+++ build.sbt
@@ -9,3 +9,4 @@ lazy val root = (project in file("."))
 libraryDependencies += "org.apache.spark" %% "spark-core" % "3.3.1"
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.3.1"
 libraryDependencies += "org.postgresql" % "postgresql" % "42.5.1"
+libraryDependencies += "io.minio" % "minio" % "8.5.1"
--- src/main/scala/BatchProcessing.scala
+++ src/main/scala/BatchProcessing.scala
@@ -1,6 +1,8 @@
 import org.apache.spark.sql.SparkSession
+import io.minio.MinioClient
+import io.minio.ListObjectsArgs
 
-class BatchProcessing {
+object BatchProcessing {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession
       .builder
@@ -9,5 +11,17 @@ class BatchProcessing {
       .appName("BatchProcessing")
       .getOrCreate()
     spark.sparkContext.setLogLevel("ERROR")
+
+    val minioClient = MinioClient.builder()
+      .endpoint("http://127.0.0.1:9000")
+      .credentials(sys.env("MINIO_ACCESS_KEY"), sys.env("MINIO_SECRET_KEY"))
+      .build()
+    val objects = minioClient.listObjects(ListObjectsArgs.builder().bucket("log-files").build())
+    val objects_iter = objects.iterator()
+    while (objects_iter.hasNext()) {
+      val objectInfo = objects_iter.next()
+      println(objectInfo.toString)
+    }
+
   }
 }
