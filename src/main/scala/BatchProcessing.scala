71b50ddb86fa0dce825469edc3fef9b095acc41d
tylerfaulkner <tylersfaulkner@gmail.com>
2023-01-19 13:11:05 -0600

    Clean and Uploads data to Minio

--- src/main/scala/BatchProcessing.scala
+++ src/main/scala/BatchProcessing.scala
@@ -23,22 +23,25 @@ object BatchProcessing {
     spark.sparkContext.hadoopConfiguration.set("fs.s3a.connection.ssl.enabled", "false")
 
     println("Getting Minio Files")
-    val data = spark.read.json("s3a://log-files/*")
-    val spamEmails = data.where(col("email_id") === "spam").select(col("email_id"), col("label"))
+    val data = spark.read.json("s3a://log-files/*.json")
+    val spamEmails = data.where(col("label") === "spam").select(col("email_id"), col("label"))
     println("Getting Postgres Data...")
     val jdbcDF = spark.read
       .format("jdbc")
       .option("url", "jdbc:postgresql://127.0.0.1:5432/email_ingestion")
       .option("dbtable", "emails")
-      .option("user", "ingestion_service")
-      .option("password", "puppet-soil-SWEETEN")
+      .option("user", System.getenv("POSTGRES_USERNAME"))
+      .option("password", System.getenv("POSTGRES_PASSWORD"))
       .load()
 
     val needed_data = jdbcDF.select(col("email_id"), col("email_object"))
 
-    val leftJoin = needed_data.join(spamEmails, needed_data("email_id") === spamEmails("email_id"), "left_outer")
+    val leftJoin = needed_data.join(spamEmails, Seq("email_id"), "left")
 
-    leftJoin.show()
-
+    val cleaned = leftJoin.na.fill("ham", Seq("label"))
+    cleaned.show()
+    cleaned.write
+      .option("fs.s3a.committer.name", "partitioned")
+      .json("s3a://training-data/data")
   }
 }
